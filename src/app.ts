97a9b0f7be9b8374fa35966cc3b11418014e50ee
Raul Ceron <receronp@gmail.com>
2023-04-12 17:15:57 -0600

    Adds getter and setter to accounting class.

--- src/app.ts
+++ src/app.ts
@@ -24,12 +24,30 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found");
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please pass in a valid value!");
+    }
+    this.addReport(value);
+  }
+
   constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
+    this.lastReport = reports[0];
   }
 
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
 
   printReports() {
@@ -49,7 +67,9 @@ console.log(it);
 
 const accounting = new AccountingDepartment("d2", []);
 
+accounting.mostRecentReport = "Year End Report";
 accounting.addReport("Something went wrong...");
+console.log(accounting.mostRecentReport);
 
 accounting.printReports();
 
