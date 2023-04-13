54a7820532355eebe9e74b0fff1fdfbe3a46ad60
Raul Ceron <receronp@gmail.com>
2023-04-13 11:47:11 -0600

    Adds generic function with interface constraint.

--- src/app.ts
+++ src/app.ts
@@ -16,3 +16,17 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergedObject = merge({ name: "Max" }, { age: 30 });
 console.log(mergedObject.age);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value";
+  if (element.length > 0) {
+    descriptionText = `Got ${element.length} element(s)`;
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe(["Sports", "Cooking"]));
