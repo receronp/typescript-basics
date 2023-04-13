a888b2636b96d769305e4610de59398ecf2ce11b
Raul Ceron <receronp@gmail.com>
2023-04-13 11:28:53 -0600

    Adds basic generic function with object constraint.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
-const names: Array<string> = [];
+// const names: Array<string> = [];
 
-const promise = new Promise<number>((resolve) => {
-  setTimeout(() => {
-    () => resolve(10);
-  }, 2000);
-});
+// const promise = new Promise<number>((resolve) => {
+//   setTimeout(() => {
+//     () => resolve(10);
+//   }, 2000);
+// });
 
-promise.then(() => {
-  // data.split(" ");
-});
+// promise.then(() => {
+//   // data.split(" ");
+// });
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObject = merge({ name: "Max" }, { age: 30 });
+console.log(mergedObject.age);
