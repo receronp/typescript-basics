9c84cbb226830fdc390a616bc7e5826a54c7a347
Raul Ceron <receronp@gmail.com>
2023-04-13 12:12:47 -0600

    Adds generic utility types (Partial/Readonly).

--- src/app.ts
+++ src/app.ts
@@ -72,3 +72,25 @@ const numberStorage = new DataStorage<number>();
 // objStorage.addItem({ name: "Anna" });
 // objStorage.removeItem({ name: "Max" });
 // console.log(objStorage.getItems());
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  completeUntil: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = completeUntil;
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ["Max", "Anna"];
+// names.push("John");
+// names.pop();
